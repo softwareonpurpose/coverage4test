c9859888e402b8fff2aa0487e7d7366849cf818d
Craig A. Stockton <craig.a.stockton@gmail.com>
2017-10-30 20:44:29 -0700

    Implement saving of report file

--- src/main/java/com/softwareonpurpose/traceability4test/CoverageReport.java
+++ src/main/java/com/softwareonpurpose/traceability4test/CoverageReport.java
@@ -1,5 +1,8 @@
 package com.softwareonpurpose.traceability4test;
 
+import java.io.File;
+import java.io.IOException;
+
 @SuppressWarnings("WeakerAccess")
 public class CoverageReport {
     public static CoverageReport getInstance() {
@@ -7,6 +10,16 @@ public class CoverageReport {
     }
 
     public void write(String filename) {
-
+        File file = new File(filename);
+        if(file.exists()){
+            //noinspection ResultOfMethodCallIgnored
+            file.delete();
+        }
+        try {
+            //noinspection ResultOfMethodCallIgnored
+            file.createNewFile();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
