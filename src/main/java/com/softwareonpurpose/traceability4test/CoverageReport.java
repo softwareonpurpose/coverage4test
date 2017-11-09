7c9e5fd5010f510801d823dfed1998f04a47e41e
Craig A. Stockton <craig.a.stockton@gmail.com>
2017-11-09 14:35:47 -0800

    Implement writing report file

--- src/main/java/com/softwareonpurpose/traceability4test/CoverageReport.java
+++ src/main/java/com/softwareonpurpose/traceability4test/CoverageReport.java
@@ -1,24 +1,29 @@
 package com.softwareonpurpose.traceability4test;
 
 import com.softwareonpurpose.indentmanager.IndentManager;
+import org.slf4j.LoggerFactory;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 @SuppressWarnings("WeakerAccess")
 public class CoverageReport {
     private static CoverageReport instance;
-    private StringBuilder content = new StringBuilder("TRACEABILITY REPORT:");
+    private final String filename;
     private List<String> requirementVerifications = new ArrayList<>();
     private IndentManager indentManager = IndentManager.getInstance();
 
-    private CoverageReport() {
+    private CoverageReport(String filename) {
+        this.filename = filename;
         indentManager.increment();
     }
 
-    public static CoverageReport getInstance() {
+    public static CoverageReport getInstance(String filename) {
         if (instance == null) {
-            instance = new CoverageReport();
+            instance = new CoverageReport(filename);
         }
         return instance;
     }
@@ -27,22 +32,59 @@ public class CoverageReport {
         instance = null;
     }
 
-    public String construct() {
-        compileVerifications();
-        return content.toString();
-    }
-
-    void compileVerifications() {
+    @Override
+    public String toString() {
+        StringBuilder content = new StringBuilder("TRACEABILITY REPORT:");
         for (String verification : requirementVerifications) {
             String test = indentManager.format(verification);
             String contentLine = String.format("%n%s", test);
             content.append(contentLine);
         }
+        return content.toString();
     }
 
     public void addEntry(String test) {
         if (!requirementVerifications.contains(test)) {
             requirementVerifications.add(test);
+        }
+    }
+
+    public void write() {
+        deleteReportFile();
+        createReportFile();
+        writeToReportFile();
+    }
+
+    private void writeToReportFile() {
+        File file = new File(filename);
+        try {
+            FileWriter writer = new FileWriter(file);
+            writer.write(this.toString());
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void createReportFile() {
+        File file = new File(filename);
+        try {
+            if (file.createNewFile()) {
+                String errorMessage = String.format("Unable to create report file %s", filename);
+                LoggerFactory.getLogger(this.getClass()).error(errorMessage);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void deleteReportFile() {
+        File file = new File(filename);
+        if (file.exists()) {
+            if (!file.delete()) {
+                String errorMessage = String.format("Unable to delete report file %s", filename);
+                LoggerFactory.getLogger(this.getClass()).error(errorMessage);
+            }
         }
     }
 }
--- src/test/java/com/softwareonpurpose/traceability4test/CoverageReportTest.java
+++ src/test/java/com/softwareonpurpose/traceability4test/CoverageReportTest.java
@@ -1,44 +1,72 @@
 package com.softwareonpurpose.traceability4test;
 
+import org.slf4j.LoggerFactory;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 @Test
 public class CoverageReportTest {
+    private final static String filename = "coverage.rpt";
 
     @Test
-    public void construct_TestOnly_single() {
+    public void write_fileCreated() {
+        CoverageReport.reset();
+        deleteReportFile();
+        CoverageReport.getInstance(filename).write();
+        Assert.assertTrue(new File(filename).exists(), "Failed to save report file");
+    }
+
+    @Test(dependsOnMethods = "write_fileCreated")
+    public void write_content() {
+        String expected = "TRACEABILITY REPORT:";
+        CoverageReport.reset();
+        CoverageReport.getInstance(filename).write();
+        String actual = readReportFile();
+        Assert.assertEquals(actual, expected, "Failed to write content to report file");
+    }
+
+    @Test
+    public void construct_testOnly_single() {
         CoverageReport.reset();
         String test = "Test.method";
         String expected = String.format("TRACEABILITY REPORT:%n  %s", test);
-        CoverageReport coverageReport = CoverageReport.getInstance();
+        CoverageReport coverageReport = CoverageReport.getInstance(filename);
         coverageReport.addEntry(test);
-        String actual = coverageReport.construct();
+        coverageReport.write();
+        String actual = readReportFile();
         Assert.assertEquals(actual, expected, "Report content failed to be compiled correctly");
     }
 
     @Test
-    public void construct_TestOnly_multiple() {
+    public void construct_testOnly_multiple() {
         CoverageReport.reset();
         String test_1 = "Test.method_1";
         String test_2 = "Test.method_2";
         String expected = String.format("TRACEABILITY REPORT:%n  %s%n  %s", test_1, test_2);
-        CoverageReport coverageReport = CoverageReport.getInstance();
+        CoverageReport coverageReport = CoverageReport.getInstance(filename);
         coverageReport.addEntry(test_1);
         coverageReport.addEntry(test_2);
-        String actual = coverageReport.construct();
+        coverageReport.write();
+        String actual = readReportFile();
         Assert.assertEquals(actual, expected, "Report content failed to be compiled correctly");
     }
 
     @Test
-    public void construct_TestOnly_duplicate() {
+    public void construct_testOnly_duplicate() {
         CoverageReport.reset();
         String test_1 = "Test.method_1";
         String expected = String.format("TRACEABILITY REPORT:%n  %s", test_1);
-        CoverageReport coverageReport = CoverageReport.getInstance();
+        CoverageReport coverageReport = CoverageReport.getInstance(filename);
         coverageReport.addEntry(test_1);
         coverageReport.addEntry(test_1);
-        String actual = coverageReport.construct();
+        coverageReport.write();
+        String actual = readReportFile();
         Assert.assertEquals(actual, expected, "Report content failed to be compiled correctly");
     }
 
@@ -46,9 +74,29 @@ public class CoverageReportTest {
     public void reset() {
         CoverageReport.reset();
         String expected = "TRACEABILITY REPORT:";
-        CoverageReport.getInstance().addEntry("test");
+        CoverageReport.getInstance(filename).addEntry("test");
         CoverageReport.reset();
-        String actual = CoverageReport.getInstance().construct();
+        String actual = CoverageReport.getInstance(filename).toString();
         Assert.assertEquals(actual, expected, "Report failed to resetContent to new content");
+    }
+
+    private String readReportFile() {
+        byte[] bytes = new byte[0];
+        try {
+            bytes = Files.readAllBytes(Paths.get(filename));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return new String(bytes, StandardCharsets.UTF_8);
+    }
+
+    private void deleteReportFile() {
+        File file = new File(filename);
+        if (file.exists()) {
+            if (!file.delete()) {
+                String errorMessage = String.format("Unable to delete report file %s", filename);
+                LoggerFactory.getLogger(this.getClass()).error(errorMessage);
+            }
+        }
     }
 }
