166f01f799a5e503695ed789e1418023c27d919e
Craig A. Stockton <craig.a.stockton@gmail.com>
2017-11-12 17:47:56 -0800

    Add multi-line formatting

--- src/main/java/com/softwareonpurpose/traceability4test/CoverageReport.java
+++ src/main/java/com/softwareonpurpose/traceability4test/CoverageReport.java
@@ -8,44 +8,45 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @SuppressWarnings("WeakerAccess")
 public class CoverageReport {
-    protected final static String reportTitle = "TRACEABILITY REPORT:%n";
-    private static CoverageReport instance;
+    protected final static String reportTitle = "TRACEABILITY REPORT:";
     private final String filename;
-    private List<String> requirementVerifications = new ArrayList<>();
+    private List<String> testScenarios = new ArrayList<>();
     private IndentManager indentManager = IndentManager.getInstance();
 
     private CoverageReport(String filename) {
         this.filename = filename;
-        indentManager.increment();
     }
 
     public static CoverageReport getInstance(String filename) {
-        if (instance == null) {
-            instance = new CoverageReport(filename);
-        }
-        return instance;
-    }
-
-    public static void reset() {
-        instance = null;
+        return new CoverageReport(filename);
     }
 
     public String compile() {
-        StringBuilder content = new StringBuilder(reportTitle);
-        for (String verification : requirementVerifications) {
-            String test = indentManager.format(verification);
-            String contentLine = String.format("%n%s", test);
+        StringBuilder content = new StringBuilder(String.format("%s%n", reportTitle));
+        List<String> sorted = testScenarios.stream().sorted().collect(Collectors.toList());
+        for (String testScenario : sorted) {
+            String[] testParts = testScenario.split("\\|");
+            indentManager.increment(2);
+            String test = indentManager.format(testParts[0]);
+            indentManager.increment();
+            String scenario = testParts.length > 1 ? indentManager.format(testParts[1]) : "";
+            indentManager.decrement();
+            indentManager.decrement();
+            indentManager.decrement();
+            String contentLine = String.format("%n%s%n%s", test, scenario);
             content.append(contentLine);
         }
         return content.toString();
     }
 
-    public void addEntry(String test) {
-        if (!requirementVerifications.contains(test)) {
-            requirementVerifications.add(test);
+    public void addEntry(String test, String scenario) {
+        String entry = String.format("%s|%s", test, scenario);
+        if (!testScenarios.contains(entry)) {
+            testScenarios.add(entry);
         }
     }
 
--- src/test/java/com/softwareonpurpose/traceability4test/CoverageReportTest.java
+++ src/test/java/com/softwareonpurpose/traceability4test/CoverageReportTest.java
@@ -2,6 +2,7 @@ package com.softwareonpurpose.traceability4test;
 
 import org.slf4j.LoggerFactory;
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import java.io.File;
@@ -9,14 +10,23 @@ import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
 
 @Test
 public class CoverageReportTest {
     private final static String filename = "coverage.rpt";
 
+    @DataProvider
+    public static Object[][] scenarios() {
+        String scenario_1 = "scenario_1";
+        String scenario_2 = "scenario_2";
+        List<String> expectedOrder = Arrays.asList(scenario_1, scenario_2);
+        return new Object[][]{{scenario_1, scenario_2, expectedOrder}, {scenario_2, scenario_1, expectedOrder}};
+    }
+
     @Test
     public void write_fileCreated() {
-        CoverageReport.reset();
         deleteReportFile();
         CoverageReport.getInstance(filename).write();
         Assert.assertTrue(new File(filename).exists(), "Failed to save report file");
@@ -24,8 +34,7 @@ public class CoverageReportTest {
 
     @Test(dependsOnMethods = "write_fileCreated")
     public void write_content() {
-        String expected = String.format("%s", CoverageReport.reportTitle);
-        CoverageReport.reset();
+        String expected = String.format("%s%n", CoverageReport.reportTitle);
         CoverageReport.getInstance(filename).write();
         String actual = readReportFile();
         Assert.assertEquals(actual, expected, "Failed to write content to report file");
@@ -33,25 +42,28 @@ public class CoverageReportTest {
 
     @Test
     public void construct_testOnly_single() {
-        CoverageReport.reset();
-        String test = "Test.method";
-        String expected = String.format("%s%n  %s", CoverageReport.reportTitle, test);
+        String test = "Test";
+        String scenario = "scenario";
+        String expected = String.format("%s%n%n    %s%n      %s", CoverageReport.reportTitle, test, scenario);
         CoverageReport coverageReport = CoverageReport.getInstance(filename);
-        coverageReport.addEntry(test);
+        coverageReport.addEntry(test, scenario);
         coverageReport.write();
         String actual = readReportFile();
         Assert.assertEquals(actual, expected, "Report content failed to be compiled correctly");
     }
 
-    @Test
-    public void construct_testOnly_multiple() {
-        CoverageReport.reset();
-        String test_1 = "Test.method_1";
-        String test_2 = "Test.method_2";
-        String expected = String.format("%s%n  %s%n  %s", CoverageReport.reportTitle, test_1, test_2);
+    @Test(dataProvider = "scenarios")
+    public void construct_testOnly_multiple(String scenario_1, String scenario_2, List<String> expectedOrder) {
+        String test_1 = "Test";
+        String sortedScenario_1 = expectedOrder.get(0);
+        String test_2 = "Test";
+        String sortedScenario_2 = expectedOrder.get(1);
+        String expectedFormat = "%s%n%n    %s%n      %s%n    %s%n      %s";
+        String expected = String.format(expectedFormat, CoverageReport.reportTitle, test_1, sortedScenario_1, test_2,
+                sortedScenario_2);
         CoverageReport coverageReport = CoverageReport.getInstance(filename);
-        coverageReport.addEntry(test_1);
-        coverageReport.addEntry(test_2);
+        coverageReport.addEntry(test_1, scenario_1);
+        coverageReport.addEntry(test_2, scenario_2);
         coverageReport.write();
         String actual = readReportFile();
         Assert.assertEquals(actual, expected, "Report content failed to be compiled correctly");
@@ -59,25 +71,15 @@ public class CoverageReportTest {
 
     @Test
     public void construct_testOnly_duplicate() {
-        CoverageReport.reset();
-        String test_1 = "Test.method_1";
-        String expected = String.format("%s%n  %s", CoverageReport.reportTitle, test_1);
+        String test = "Test";
+        String scenario = "scenario";
+        String expected = String.format("%s%n%n    %s%n      %s", CoverageReport.reportTitle, test, scenario);
         CoverageReport coverageReport = CoverageReport.getInstance(filename);
-        coverageReport.addEntry(test_1);
-        coverageReport.addEntry(test_1);
+        coverageReport.addEntry(test, scenario);
+        coverageReport.addEntry(test, scenario);
         coverageReport.write();
         String actual = readReportFile();
         Assert.assertEquals(actual, expected, "Report content failed to be compiled correctly");
-    }
-
-    @Test
-    public void reset() {
-        CoverageReport.reset();
-        String expected = String.format("%s", CoverageReport.reportTitle);
-        CoverageReport.getInstance(filename).addEntry("test");
-        CoverageReport.reset();
-        String actual = CoverageReport.getInstance(filename).compile();
-        Assert.assertEquals(actual, expected, "Report failed to resetContent to new content");
     }
 
     private String readReportFile() {
