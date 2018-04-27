d93a43d0b71a7007bd4896ac85942ed2ce666a6a
Craig A. Stockton <craig.a.stockton@gmail.com>
2018-04-26 21:33:02 -0700

    Refactoring of content variable to private field

--- src/main/java/com/softwareonpurpose/traceability4test/CoverageReport.java
+++ src/main/java/com/softwareonpurpose/traceability4test/CoverageReport.java
@@ -24,6 +24,15 @@ import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
+/***
+ * CoverageReport accepts entries for each test executed, including the test description, scenario description,
+ * and requirement description.  Only the test description is required.
+ * A requirement can be a combination of Inter-application requirement (crosses application boundaries)
+ * and Intra-application requirement, pipe separated (i.e. [Inter-application|Intra-application]).
+ * A CoverageReport is instantiated with the name of the file to which the report is to be written.
+ * The list of entries is generated as a report, and written to the filename provided.
+ * The report is aggregated by inter-application requirement, intra-application requirement, scenario, and test.
+ */
 @SuppressWarnings("WeakerAccess")
 public class CoverageReport {
     protected final static String reportTitle = "TRACEABILITY REPORT:";
@@ -42,17 +51,27 @@ public class CoverageReport {
     private List<String> reportedTests = new ArrayList<>();
     private List<String> reportedIntraSystemRequirements = new ArrayList<>();
     private List<String> reportedInterSystemRequirements = new ArrayList<>();
+    private StringBuilder compiledContent;
 
     private CoverageReport(String target) {
         this.filename = String.format("%s.coverage.rpt", target);
     }
 
+    /***
+     * Get an instance of CoverageReport
+     * @param filename Name of file to which report will be written
+     * @return Instance of CoverageReport
+     */
     public static CoverageReport getInstance(String filename) {
         return new CoverageReport(filename);
     }
 
+    /***
+     * Compile the lists of Requirements, Scenarios and Tests into content for a coverage report
+     * @return String containing formatted coverage report
+     */
     public String compile() {
-        StringBuilder content = new StringBuilder(String.format(TITLE_FORMAT, reportTitle));
+        compiledContent = new StringBuilder(String.format(TITLE_FORMAT, reportTitle));
         List<String> sorted = testScenarios.stream().sorted().collect(Collectors.toList());
         for (String testScenario : sorted) {
             String[] testParts = testScenario.split("\\|");
@@ -61,7 +80,7 @@ public class CoverageReport {
             if (isInterSystemRequirementAvailable && !reportedInterSystemRequirements.contains
                     (interSystemRequirement)) {
                 reportedIntraSystemRequirements.clear();
-                content.append(String.format(INTER_SYSTEM_FORMAT, interSystemRequirement));
+                compiledContent.append(String.format(INTER_SYSTEM_FORMAT, interSystemRequirement));
                 reportedInterSystemRequirements.add(interSystemRequirement);
             }
             String intraSystemRequirement = testParts[INTRA_SYSTEM_REQUIREMENT_INDEX];
@@ -69,23 +88,29 @@ public class CoverageReport {
             if (isIntraSystemRequirementAvailable && !reportedIntraSystemRequirements.contains
                     (intraSystemRequirement)) {
                 reportedTests.clear();
-                content.append(String.format(INTRA_SYSTEM_FORMAT, intraSystemRequirement));
+                compiledContent.append(String.format(INTRA_SYSTEM_FORMAT, intraSystemRequirement));
                 reportedIntraSystemRequirements.add(intraSystemRequirement);
             }
             String test = testParts[TEST_INDEX];
             if (!reportedTests.contains(test)) {
-                content.append(String.format(TEST_FORMAT, test));
+                compiledContent.append(String.format(TEST_FORMAT, test));
                 reportedTests.add(test);
             }
             String scenario = testParts[SCENARIO_INDEX];
             boolean isScenarioAvailable = !NOT_AVAILABLE.equals(scenario);
             if (isScenarioAvailable) {
-                content.append(String.format(SCENARIO_FORMAT, scenario));
+                compiledContent.append(String.format(SCENARIO_FORMAT, scenario));
             }
         }
-        return content.toString();
+        return compiledContent.toString();
     }
 
+    /***
+     * Add an entry for a test, the scenario in which it was executed, and the requirement covered
+     * @param test Name of the test executed
+     * @param scenario Description of the data scenario in which the test was executed
+     * @param requirement Description of the requirement covered
+     */
     public void addEntry(String test, String scenario, String requirement) {
         String validatedRequirement = nullToNa(requirement);
         validatedRequirement = validatedRequirement.contains("|") ? validatedRequirement : String.format("%s|%s",
@@ -97,6 +122,12 @@ public class CoverageReport {
         }
     }
 
+    /***
+     * Add entry covering multiple requirements
+     * @param test Name of test executed
+     * @param scenario Description of the data scenario in which the test was executed
+     * @param requirements Comma-delimited list of descriptions of requirements covered
+     */
     public void addEntries(String test, String scenario, String requirements) {
         List<String> requirementList = requirements == null ? Collections.singletonList(null) : parseRequirements
                 (requirements);
@@ -121,19 +152,16 @@ public class CoverageReport {
     }
 
     private List<String> extractRequirements(String requirements) {
-        List<String> requirementList;
-        if (requirements == null) {
-            requirementList = Collections.singletonList(null);
-        } else {
-            requirementList = Arrays.stream(requirements.split("\\|")).collect(Collectors.toList());
-        }
-        return requirementList;
+        return requirements == null ? Collections.singletonList(null) : Arrays.stream(requirements.split("\\|")).collect(Collectors.toList());
     }
 
     private String nullToNa(String scenario) {
         return scenario == null ? String.format("%s", NOT_AVAILABLE) : scenario;
     }
 
+    /***
+     * Write the coverage report to file.  Any existing file with the same name will be deleted.
+     */
     public void write() {
         deleteReportFile();
         createReportFile();
